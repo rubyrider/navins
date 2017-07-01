8a62e1ec84e3164b9197ccf92da6ced139bcd3d7
Irfan Ahmed <irfan@lesouk.co>
2017-07-01 17:44:34 +0600

    Initial annotation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: conversations
+#
+#  id           :integer          not null, primary key
+#  recipient_id :integer
+#  sender_id    :integer
+#  subject      :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Conversation < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: :sender_id, optional: true
   belongs_to :receiver, class_name: 'User', foreign_key: :recipient_id, optional: true
